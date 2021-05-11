8afd5e349da4b5daa5d1d01a7f468342c16de8aa
yhewtifa1a97 <ubr7081895118076@163.com>
2021-05-11 14:09:39 +0800

    fix bug

--- app/src/main/java/com/atom/shuoshuo/ui/activity/MyBlogActivity.java
+++ app/src/main/java/com/atom/shuoshuo/ui/activity/MyBlogActivity.java
@@ -1,11 +1,17 @@
 package com.atom.shuoshuo.ui.activity;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.webkit.WebView;
 
 import com.atom.shuoshuo.R;
 import com.atom.shuoshuo.base.AbsBaseActivity;
+
+import butterknife.BindView;
 
 /**
  * ============================================================
@@ -21,6 +27,11 @@ import com.atom.shuoshuo.base.AbsBaseActivity;
  **/
 public class MyBlogActivity extends AbsBaseActivity {
 
+    @BindView(R.id.toolbar)
+    Toolbar mToolbar;
+    @BindView(R.id.wv_blog)
+    WebView mWvBlog;
+
     public static void startActivity(Context context) {
         Intent intent = new Intent(context, MyBlogActivity.class);
         context.startActivity(intent);
@@ -33,8 +44,17 @@ public class MyBlogActivity extends AbsBaseActivity {
     }
 
     @Override
+    @SuppressLint("SetJavaScriptEnabled")
     protected void initViews(Bundle savedInstanceState) {
-
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        mToolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
+            }
+        });
+        mWvBlog.getSettings().setJavaScriptEnabled(true);
+        mWvBlog.loadUrl("http://www.aiwuol.xyz");
     }
-
 }
